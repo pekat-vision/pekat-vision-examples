b39259b0ed7e3807fa77029dca74e37a31790574
PEKAT VISION <team@pekatvision.com>
2018-06-07 19:39:43 +0200

    Adding Basler camera driver example.

---
+++ camera_basler/README.md
@@ -0,0 +1,9 @@
+# Camera Basler
+
+This example demonstrates how to connect Basler cameras to PEKAT VISION.
+
+Requirements
+ - Basler's software - Pylon SDK
+ - Python wrapper around Pylon - PyPylon
+
+Usage: In Pylon Viewer configure your camera and save the settings into a file. The example shows how to read the file with PyPylon, how to grab frames from the camera through the PyPylon and how to send the frames to PEKAT VISION.
---
+++ camera_basler/basler_driver_http_raw.py
@@ -0,0 +1,35 @@
+
+# Requirements:
+#
+#  - Install Pylon from https://www.baslerweb.com/en/sales-support/downloads/software-downloads/#os=linuxx86;version=all;type=pylonsoftware;language=all
+#  - Install PyPylon from https://github.com/basler/pypylon
+
+from pypylon import pylon
+import requests
+
+url = 'http://127.0.0.1:8100/analyze_raw_image'
+
+# Init camera - load settings from file. You can get such file from Pylon Viewer in menu Camera -> Save features...
+camera_settings_file = "camera_settings.pfs"  # name of the file
+
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+camera.Open()
+pylon.FeaturePersistence_Load(camera_settings_file, camera.GetNodeMap(), True)
+
+
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grabResult.GrabSucceeded():
+        frame = grabResult.Array
+        shape = frame.shape
+
+        # Important! Set Pixel Format in the camera to BGR 8 (for a color image) otherwise you would need to convert the image here.
+        u = url+'?width=' + str(shape[1]) + '&height=' + str(shape[0])
+        r = requests.post(url=u, data=frame.tobytes(), headers={'Content-Type': 'application/octet-stream'})
+
+        print(r.json())
+
+    grabResult.Release()
